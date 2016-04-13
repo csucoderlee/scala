1e08538a9f20baa8b584ac0551f369595345a54f
lixiang <csucoderlee@foxmail.com>
2016-04-13 09:55:13 +0800

    first commit helloworld.scala

---
+++ src/org/csu/coderlee/Hello.scala
@@ -0,0 +1,10 @@
+package org.csu.coderlee
+
+/**
+  * Created by lixiang on 16/4/9.
+  */
+class Hello {
+  def main(args:Array[String]): Unit = {
+    println("hello world")
+  }
+}
---
+++ src/org/csu/coderlee/HelloWorld.scala
@@ -0,0 +1,10 @@
+package org.csu.coderlee
+
+/**
+  * Created by lixiang on 16/4/13.
+  */
+object HelloWorld {
+  def main(args:Array[String]): Unit = {
+    println("hello world")
+  }
+}
